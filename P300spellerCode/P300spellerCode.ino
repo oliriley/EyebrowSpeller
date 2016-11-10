7fc37dabb880e4b2765b16424a6233672f5adde3
Oliver <oliriley@uw.edu>
2016-11-09 21:03:53 -0500

    Fixed display bug and added message recording
    TODO: Incorporate cases for CAPS, DEL, START, STOP

--- P300spellerCode/P300spellerCode.ino
+++ P300spellerCode/P300spellerCode.ino
@@ -1,79 +1,188 @@
 int xAccelPin = A0;
 int yAccelPin = A1;
-//consider taking magnitude of X and Y together
-const int ARRAY_SIZE = 150;
-double THRESH = 0.03;
 
+const int ARRAY_SIZE = 10;
+const int COLS = 6;
+double THRESH = 0.05;
 
 double magVals[ARRAY_SIZE];
 double magAvg = 0.0;
 double magDiff = 0.0;
 
+unsigned long millisNow;
 
-long int millisNow;
+String message = "Message: ";
 
+bool CAPS = true;
+bool AVGD = false;
 
-int counter = 0;
-//int row = 0; //use for cycling throgh rows later
-//int col = 0; //use for cycling throgh columns later
+int counter = 0; // keep track of how many magnitude samples have been recorded
+int vector = 0; // use for cycling through highlighted column or row; 6 columns, 7 rows
+int row = -1; //use for saving selected row later
+int col = -1; //use for saving selected column later
 
+const char capChars[][COLS] = {{'_', 'E', 'A', 'H', 'R', 'W'},{'T', '<', 'I', 'L', 'M', 'K'},{'N', 'O', 'C', 'G', 'V', 'X'},{'S', 'D', 'Y', 'B', 'J', '?'},{'U', 'F', 'P', 'Q', 'Z', '.'},{'1', '2', '3', '4', '5', '6'},{'7', '8', '9', '0', '^', '!'}};
+const char lowChars[][COLS] = {{'_', 'e', 'a', 'h', 'r', 'w'},{'t', '<', 'i', 'l', 'm', 'k'},{'n', 'o', 'c', 'g', 'v', 'x'},{'s', 'd', 'y', 'b', 'j', '?'},{'u', 'f', 'p', 'q', 'z', '.'},{'1', '2', '3', '4', '5', '6'},{'7', '8', '9', '0', '^', '!'}};
 
 void setup() {
   //set up pin configurations
   pinMode(xAccelPin, INPUT);
   pinMode(yAccelPin, INPUT);
-
-
+  
   //start serial
   Serial.begin(9600);
-
-
+  
   for (int index = 0; index < ARRAY_SIZE; index++){
     magVals[index] = 0;
   }
+
+  millisNow = millis();
 }
 
-
-void loop() {
+void loop(){
+  if (magAvg != 0 && (millis()-millisNow) > 1000){ //highlight next column or row only after 1 sec
+    Serial.println(message);
+    Serial.println();
+    millisNow = millis();
+    if(vector == 0) //highlight column 1
+    {
+      Serial.println("[_] E A H R W\n[T] < I L M K\n[N] O C G V X\n[S] D Y B J ?\n[U] F P Q Z .\n[1] 2 3 4 5 6\n[7] 8 9 0 ^ !");
+    }
+    else if(vector == 1) //highlight column 2
+    {
+      Serial.println("_ [E] A H R W\nT [<] I L M K\nN [O] C G V X\nS [D] Y B J ?\nU [F] P Q Z .\n1 [2] 3 4 5 6\n7 [8] 9 0 ^ !");
+    }
+    else if(vector == 2) //highlight column 3
+    {
+      Serial.println("_ E [A] H R W\nT < [I] L M K\nN O [C] G V X\nS D [Y] B J ?\nU F [P] Q Z .\n1 2 [3] 4 5 6\n7 8 [9] 0 ^ !");
+    }
+    else if(vector == 3) //highlight column 4
+    {
+      Serial.println("_ E A [H] R W\nT < I [L] M K\nN O C [G] V X\nS D Y [B] J ?\nU F P [Q] Z .\n1 2 3 [4] 5 6\n7 8 9 [0] ^ !");
+    }
+    else if(vector == 4) //highlight column 5
+    {
+      Serial.println("_ E A H [R] W\nT < I L [M] K\nN O C G [V] X\nS D Y B [J] ?\nU F P Q [Z] .\n1 2 3 4 [5] 6\n7 8 9 0 [^] !");
+    }
+    else if(vector == 5) //highlight column 6
+    {
+      Serial.println("_ E A H R [W]\nT < I L M [K]\nN O C G V [X]\nS D Y B J [?]\nU F P Q Z [.]\n1 2 3 4 5 [6]\n7 8 9 0 ^ [!]");
+    }
+    else if(vector == 6) //highlight row 1
+    {
+      Serial.println("[_][E][A][H][R][W]\nT < I L M K\nN O C G V X\nS D Y B J ?\nU F P Q Z .\n1 2 3 4 5 6\n7 8 9 0 ^ !");
+    }
+    else if(vector == 7) //highlight row 2
+    {
+      Serial.println("_ E A H R W\n[T][<][I][L][M][K]\nN O C G V X\nS D Y B J ?\nU F P Q Z .\n1 2 3 4 5 6\n7 8 9 0 ^ !");
+    }
+    else if(vector == 8) //highlight row 3
+    {
+      Serial.println("_ E A H R W\nT < I L M K\n[N][O][C][G][V][X]\nS D Y B J ?\nU F P Q Z .\n1 2 3 4 5 6\n7 8 9 0 ^ !");
+    }
+    else if(vector == 9) //highlight row 4
+    {
+      Serial.println("_ E A H R W\nT < I L M K\nN O C G V X\n[S][D][Y][B][J][?]\nU F P Q Z .\n1 2 3 4 5 6\n7 8 9 0 ^ !");
+    }
+    else if(vector == 10) //highlight row 5
+    {
+      Serial.println("_ E A H R W\nT < I L M K\nN O C G V X\nS D Y B J ?\n[U][F][P][Q][Z][.]\n1 2 3 4 5 6\n7 8 9 0 ^ !");
+    }
+    else if(vector == 11) //highlight row 6
+    {
+      Serial.println("_ E A H R W\nT < I L M K\nN O C G V X\nS D Y B J ?\nU F P Q Z .\n[1][2][3][4][5][6]\n7 8 9 0 ^ !");
+    }
+    else //vector=12 - highlight row 7
+    {
+      Serial.println("_ E A H R W\nT < I L M K\nN O C G V X\nS D Y B J ?\nU F P Q Z .\n1 2 3 4 5 6\n[7][8][9][0][^][!]");
+    }
+    Serial.println();
+  }
+  
   // read the input of acceleration in x axis (as a voltage):
   double xAccelValue = analogRead(xAccelPin)*((float)5/1023);
   // read the input of acceleration in y axis (as a voltage):
   double yAccelValue = analogRead(yAccelPin)*((float)5/1023);
   magVals[counter] = sqrt(pow(xAccelValue,2) + pow(yAccelValue,2));
   magDiff = magVals[counter]-magAvg;
-  // print out the values you read:
+  // for debugging
   /*Serial.print(magDiff);
   Serial.print(magAvg);*/
 
-
-  if (magAvg != 0){
-
-
-    for (int index = 0; index < 10; index++){
-      Serial.println("");
+  if (AVGD && magDiff > THRESH)
+  {
+    Serial.println("Eyebrow moved!");
+    //Save column or row number
+    if(vector<6){
+      col = vector;
+      vector = 6; //advance to highlighting 1st row next
     }
-    Serial.println("e t a o i n s");
-    Serial.println(". ! ? < ^ _ 0");
-    Serial.println("h r d l c u m");
-    Serial.println("w f g y p b v");
-    Serial.println("k j x q z 1 2");
-    Serial.println("3 4 5 6 7 8 9");
-
-
-    if (magDiff > THRESH){
-      //
+    else{
+      row = vector - 6;
+      vector = 0; //advance to highlighting 1st column next
     }
+    Serial.print("Row = ");
+    Serial.print(row);
+    Serial.print(" Col = ");
+    Serial.println(col);
+    delay(500);
   }
   
   counter++;
-  
-  if (counter%(ARRAY_SIZE/10)==0){
+
+  //Take a running average w/ overlapping windows
+  if (AVGD && (counter%(ARRAY_SIZE/10)==0)){
     for (int index = 0; index < ARRAY_SIZE; index++){
       magAvg += magVals[index];
     }
     magAvg = magAvg / ARRAY_SIZE;
   }
+  else if (counter%ARRAY_SIZE==0){
+    AVGD = true;
+    for (int index = 0; index < ARRAY_SIZE; index++){
+      magAvg += magVals[index];
+    }
+    magAvg = magAvg / ARRAY_SIZE;
+  }
+  
   if (counter >= ARRAY_SIZE){
     counter = 0;
+  }
+  
+  else //no eyebrow movement
+  {
+    if((millis()-millisNow) > 1000) //highlight next column or row only after 1 sec
+    {
+      if(vector == 5) //reached last column
+      {
+        //cycle through columns again
+        vector = 0;
+      }
+      else if(vector == 12)
+      {
+        //cycle through rows again
+        vector = 6;
+      }
+      else
+      {
+        //advance to next column or row
+        vector++;
+      }
+    }
+  }
+  
+  // if a column and row number were selected
+  if (row!=-1 && col!=-1){
+    if (CAPS){
+      //message.concat(capChars[col][row]);
+      message += capChars[row][col];
+    }
+    else {
+      //message.concat(lowChars[col][row]);
+      message += lowChars[row][col];
+    }
+    //reset col and row
+    col = -1;
+    row = -1;
   }
 }
