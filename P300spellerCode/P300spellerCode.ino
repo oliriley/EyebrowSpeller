b8358928eefdcdc17d51f291de616f4c9b1a5197
Oliver <oliriley@uw.edu>
2016-11-15 07:21:35 -0500

    Cleaned up and removed extraneous printlns/comments; still need stop fcn

--- P300spellerCode/P300spellerCode.ino
+++ P300spellerCode/P300spellerCode.ino
@@ -1,31 +1,24 @@
-/*
-What we want the loop to do:
-Display a thing
-Take some readings
-See if those readings are bigger than a periodically calculated amount
-If enough time passes without those readings passing that amount, display a new thing
-If at any point the readings pass that amount, write down what you were displaying and display a different class of things
-If you've written down two different things, write down their combination
-*/
 int xAccelPin = A0;
 int yAccelPin = A1;
 
-const int ARRAY_SIZE = 60;
+const int ARRAY_SIZE = 50;
 const int COLS = 6;
 const int DELAY = 1000;
 
-double THRESH = 0.03;
+double THRESH = .006;
 
 double magVals[ARRAY_SIZE];
 double magAvg = 0.0;
 double magDiff = 0.0;
 
 unsigned long millisNow;
+unsigned long millisChar;
 
 String message = "";
 
 bool CAPS = true;
 bool AVGD = false;
+bool MOVE = false;
 
 int counter = 0; // keep track of how many magnitude samples have been recorded
 int vector = -1; // use for cycling through highlighted column or row; 6 columns, 7 rows
@@ -48,6 +41,7 @@ void setup() {
   }
 
   millisNow = millis();
+  millisChar = millisNow;
 }
 
 void loop(){
@@ -60,18 +54,77 @@ void loop(){
   magDiff = magVals[counter]-magAvg;
   
   counter++;
+
+  //Take a running average w/ overlapping windows
+  if (AVGD && (counter%(ARRAY_SIZE/10)==0))
+  {
+    for (int index = 0; index < ARRAY_SIZE; index++){
+      magAvg += magVals[index];
+    }
+    magAvg = magAvg / ARRAY_SIZE;
+    //Serial.println("Avging");
+  }
+  else if (counter%ARRAY_SIZE==0)
+  {
+    AVGD = true;
+    for (int index = 0; index < ARRAY_SIZE; index++){
+      magAvg += magVals[index];
+    }
+    magAvg = magAvg / ARRAY_SIZE;
+  }
+
+  if (counter >= ARRAY_SIZE){
+    counter = 0;
+  }
   
-  if (AVGD){ //highlight next column or row only after 1 sec
+  if (AVGD)
+  {
+    if ((magDiff > THRESH) && (millis() - millisChar > DELAY/2))// eyebrow movement far enough apart to matter
+    {
+      MOVE = true;
+      /*Serial.print("MagDiff: ");
+      Serial.print(magDiff);
+      Serial.print(" MagAvg: ");
+      Serial.println(magAvg);*/
+      Serial.print("Row = ");
+      Serial.print(row);
+      Serial.print(" Col = ");
+      Serial.println(col);      
+      //Save column or row number
+      if(vector<6){ // 0-5
+        col = vector;
+        vector = 6; //advance to highlighting 1st row next
+      }
+      else{ // 6-12
+        row = vector - 6;
+        vector = 0; //advance to highlighting 1st column next
+      }
+      millisChar = millis();
+    }
+    
     if (millis() - millisNow > DELAY)
     {
-      //advance to next column or row
       vector++;
+      if(vector == 6) //reached last column
+      {
+        //cycle through columns again
+        vector = 0;
+      }
+      else if(vector == 13)
+      {
+        //cycle through rows again
+        vector = 6;
+      }
+      else if(vector == 1 || vector == 7)
+      {
+        vector -= MOVE;
+      }
+
+      MOVE = false;
+
       Serial.print("Message: ");
       Serial.println(message);
       Serial.println();
-      millisNow = millis();
-      /*Serial.print("Vector: ");
-      Serial.println(vector);*/
       if(vector == 0) //highlight column 1
       {
         Serial.println("[_] E A H R W\n[T] < I L M K\n[N] O C G V X\n[S] D Y B J ?\n[U] F P Q Z .\n[1] 2 3 4 5 6\n[7] 8 9 0 ^ !");
@@ -125,55 +178,12 @@ void loop(){
         Serial.println("_ E A H R W\nT < I L M K\nN O C G V X\nS D Y B J ?\nU F P Q Z .\n1 2 3 4 5 6\n[7][8][9][0][^][!]");
       }
       Serial.println();
-      /*Serial.print("MagDiff: ");
-      Serial.print(magDiff);
-      Serial.print(" MagAvg: ");
-      Serial.println(magAvg);*/
+      millisNow = millis();
     }
-  
-    /*As far as I can tell, there is an issue with the logic of this IF/ELSE statement.
-    / After a column has been selected, the ELSE component gets run as well
-    / This means that the VECTOR variable starts at 7 (the second row). Setting VECTOR to
-    / -1 after selecting a row avoids this in the second case (starts on column 1), but 
-    / I'm still working on getting it to not jump a row. Workaround: move last row to top.*/
-    delay(1);
-    if (magDiff < THRESH) //no eyebrow movement
-    {
-      if((millis()-millisNow) > DELAY) //highlight next column or row only after specified amount of time sec
-      {
-        if(vector == 5) //reached last column
-        {
-          //cycle through columns again
-          vector = -1;
-        }
-        else if(vector == 12)
-        {
-          //cycle through rows again
-          vector = 5;
-        }
-      }
-    }
-    else
-    {
-      Serial.println("Eyebrow moved!");
-      //Save column or row number
-      if(vector<6){ // 0-5
-        col = vector;
-        vector = 5; //advance to highlighting 1st row next
-      }
-      else{ // 6-11
-        row = vector - 6;
-        vector = -1; //advance to highlighting 1st column next; -1 not 0 because bug
-      }
-      /*Serial.print("Row = ");
-      Serial.print(row);
-      Serial.print(" Col = ");
-      Serial.println(col);*/
-      delay(DELAY);
-    }
-  
+    
     // if a column and row number were selected
-    if (row!=-1 && col!=-1){
+    if (row!=-1 && col!=-1)
+    {
       if (lowChars[row][col] == '_'){ //add space
         message += ' ';
       }
@@ -204,23 +214,9 @@ void loop(){
       row = -1;
     }
   }
-  
-  //Take a running average w/ overlapping windows
-  if (AVGD && (counter%(ARRAY_SIZE/10)==0)){
-    for (int index = 0; index < ARRAY_SIZE; index++){
-      magAvg += magVals[index];
-    }
-    magAvg = magAvg / ARRAY_SIZE;
-  }
-  else if (counter%ARRAY_SIZE==0){
-    AVGD = true;
-    for (int index = 0; index < ARRAY_SIZE; index++){
-      magAvg += magVals[index];
-    }
-    magAvg = magAvg / ARRAY_SIZE;
-  }
 
-  if (counter >= ARRAY_SIZE){
-    counter = 0;
+  delay(1);
+  if (vector > 12){
+    vector = 0; //precaution
   }
 }
